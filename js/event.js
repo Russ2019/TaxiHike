ed1f548166f82e114a7c44450277f530a00f5a4b
ths93 <tfra_oslo@hotmail.com>
2016-05-10 09:05:12 +0200

    Update event.js

--- js/event.js
+++ js/event.js
@@ -1,1 +1,35 @@
-event
+    //*********************************************************    
+    // Event Capabilities
+    //*********************************************************
+    function updateEventStatus(status) {
+        $("#eventStatus").empty();
+        $("#eventStatus").append(status).trigger("create");
+    }
+
+    function batteryStatusEvt(e) {
+        alert(e.type + " event created." + "\nBattery Level: " + e.level + "\nCharger plugin? " + e.isPlugged);
+    }
+    
+    function menuEvt(e) {
+        alert(e.type + " event created.");
+    }
+
+    function toggleBatteryStatusEvent() {
+        if ( $("#flip-batteryStatus option:selected").val() == "on" ) {
+            updateEventStatus("Enable battery status event");
+            window.addEventListener("batterystatus", batteryStatusEvt, false);          
+        } else {
+            updateEventStatus("Disable battery status event");
+            window.removeEventListener("batterystatus", batteryStatusEvt, false);
+        }
+    }
+
+    function toggleMenuEvent() {
+        if ( $("#flip-menu option:selected").val() == "on" ) {
+            updateEventStatus("Enable menu button event");
+            document.addEventListener("menubutton", menuEvt, false);
+        } else {
+            updateEventStatus("Disable menu button event");
+            document.removeEventListener("menubutton", menuEvt, false);
+        }
+    }
