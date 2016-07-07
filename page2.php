fd795353ba0e6c63aed3eda2c425b2700d6341d2
ths93 <tfra_oslo@hotmail.com>
2016-07-07 21:57:41 +0200

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<title>&nbsp;</title>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<script src="https://maps.googleapis.com/maps/api/js?sensor=false&v=3&libraries=geometry"></script>
+	
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+        
+	<link rel="stylesheet" type="text/css" href="http://ququplay.github.io/jquery-mobile-square-ui-theme/css/jquery.mobile.squareui.css" />
+	<script src="http://ququplay.github.io/jquery-mobile-square-ui-theme/js/jquery.min.js"></script>
+	<script src="http://ququplay.github.io/jquery-mobile-square-ui-theme/js/jqm.min.js"></script>
+	<script type="text/javascript" charset="utf-8" src="cordova.js"></script>
+		
+	<style>
+		#content1 {
+		padding: 0;
+		position : absolute !important;
+		top : 40px !important;  
+		right : 0; 
+		bottom : 0px !important;  
+		left : 0 !important;     
+		}	
+
+                .icon-green {
+                color: #006600;
+                }
+                .icon-white {
+                color: #ffffff;
+                }
+                 	
+	        .hdr {
+                background-color: #e0e0e0;
+                }
+                .hdr1 {
+                color: #006600;
+                font-size: 32px;
+                font-weight: bold;
+                }
+                .hdr2 {
+                color: #006600;
+                font-size: 20px;
+                font-weight: normal;
+                margin-top:-5px;
+                }
+         </style>
+</head>
+
+<body>
+	page2
+</body>
+
+</html>
