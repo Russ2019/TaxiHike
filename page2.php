6c44cf5e3befd8986cc7b5c10e7bb61ea773b9d2
ths93 <tfra_oslo@hotmail.com>
2016-07-10 22:35:29 +0200

    Update page2.php

--- page2.php
+++ page2.php
@@ -21,7 +21,19 @@
 	</div><!-- /header -->
 
 	<div data-role="content">	
-		page 2...
+		<script src="http://maps.googleapis.com/maps/api/js"> </script>
+		<script>
+		  function initialize() {
+		    var mapProp = {
+		      center:new google.maps.LatLng(51.508742,-0.120850),
+		      zoom:5,
+		      mapTypeId:google.maps.MapTypeId.ROADMAP
+		    };
+		    var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+		  }
+		  google.maps.event.addDomListener(window, 'load', initialize);
+		</script>
+		<div id="googleMap" style="width:500px;height:380px;"></div>
 	</div><!-- /content -->
 	
 	<div data-role="footer">
