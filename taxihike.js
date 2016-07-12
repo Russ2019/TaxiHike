5e8f9ec3f4640aa4bb9a25516359ef6776f57fcc
ths93 <tfra_oslo@hotmail.com>
2016-07-12 11:26:31 +0200

    Update taxihike.js

--- taxihike.js
+++ taxihike.js
@@ -1,1 +1,27 @@
+		function taxihike() { alert('taxi hike'); }
+
+//-------------------------------- Local storage ---------------------------------------			
+		function local_var(key) {
+			return local_storage_get(key);
+			}
+
+		function local_storage_set(key, value) {	
+			if(typeof(Storage) !== "undefined") {
+				localStorage.setItem(key, value);
+				// alternativt: localStorage.lastname = "Smith";
+			} else {
+				return 'error local_storage_set';
+				// Sorry! No Web Storage support..
+			}
+		}
+		
+		function local_storage_get(key) {	
+			if(typeof(Storage) !== "undefined") {
+				return localStorage.getItem(key);
+				//alternativt: var lastname = localStorage.lastname;
+			} else {
+				return 'error local storage get';
+				// Sorry! No Web Storage support..
+			}
+		}
  
