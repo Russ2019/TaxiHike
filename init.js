654ba5380f32ed612ffba53bb6e263e6c46d4567
ths93 <tfra_oslo@hotmail.com>
2016-07-12 20:27:30 +0200

    Create init.js

---
+++ init.js
@@ -0,0 +1,37 @@
+		function init_app() {
+			ajax_init();
+			}		
+
+		function ajax_init() {
+			var kode = $('#kode').val();
+			$.ajax({
+				  url: 'http://itorget.no/tc/wp-content/themes/tc/server/server_tc.php',
+				  type: 'post',
+				  data : {appid : kode, srvc: 'init_taxihike'},
+				  dataType: 'json',
+				  success: function(data) {
+						initData(data);
+						},
+				  error: function(result){
+						alert('error ajax_init');
+						}
+				});			
+			}
+			
+		function initData(data) {			
+			//save to local storage...then show local_storage_show...
+			if (data !== '') {
+				var objs = data;
+				
+				$.each(objs, function(idx, obj){ 
+					$.each(obj, function(key, value){
+						local_storage_set(key, value);
+					});
+				});
+				}
+			else {
+				alert('Feil kode');
+				$('#kode').val('');
+				}			
+			}
+			
